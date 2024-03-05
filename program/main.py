c8348b5b6d37e8d1d56012353a0ebe24b8abac79
Abdelkader DALI <abdelkader.dali@icloud.com>
2024-03-05 20:26:54 +0100

    stage 3 start

--- program/main.py
+++ program/main.py
@@ -1,6 +1,7 @@
 from func_connections import connect_dydx
 from func_private import abort_all_positions
 from func_public import construct_market_prices
+from func_cointegration import store_cointegration_results
 from constants import ABORT_ALL_POSITIONS, FIND_COINTEGRATED
 
 if __name__ == '__main__':
@@ -29,4 +30,15 @@ if __name__ == '__main__':
       df_market_prices = construct_market_prices(client)
     except Exception as e:
       print(f"Error fetching market prices: {e}")
+      exit(1)
+    
+    # Store Cointegrated Pairs
+    try:
+      print("Storing cointegrated pairs ...")
+      stores_result = store_cointegration_results(df_market_prices)
+      if stores_result != "saved":
+        print("Error storing cointegrated pairs")
+        exit(1)
+    except Exception as e:
+      print(f"Error storing cointegrated pairs: {e}")
       exit(1)
